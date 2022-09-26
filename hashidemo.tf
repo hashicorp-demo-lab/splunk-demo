3eecc8860daa828fb05c96e20eeea35412fa4b0f
Cameron Huysmans <cameron@hashicorp.com>
2022-09-26 22:47:48 +1000

    Updated DNS module

--- hashidemo.tf
+++ hashidemo.tf
@@ -5,22 +5,30 @@ provider "azurerm" {
   features {}
 }
 
-module "dns" {  
-  source = "github.com/lhaig/terraform-dns-multicloud?ref=v0.3.0"
-
+module "dns" { 
+  source = "https://github.com/chuysmans/dns-multicloud"
   hosted-zone = "hashidemos.io"
-  namespace   = var.prefix                  # HashiCorp username
-  owner       = var.prefix                  # HashiCorp email (no @ symbol)
-
-  create_aws_dns_zone   = true              # should be set to true
-  
-  create_azure_dns_zone = false              # optional
-  azure_location        = "westus"          # Azure creates a resource group
-                                            #   which requires a region
-                                    
-  create_gcp_dns_zone   = false              # optional
-  gcp_project           = "robert-peteuil"  # name of your GCP project
+   namespace   = var.prefix                  # HashiCorp username
+   owner       = var.prefix                  # HashiCorp email (no @ symbol)
+   create_aws_dns_zone   = true              # should be set to true
 }
+
+# module "dns" {  
+#   source = "github.com/lhaig/terraform-dns-multicloud?ref=v0.3.0"
+
+#   hosted-zone = "hashidemos.io"
+#   namespace   = var.prefix                  # HashiCorp username
+#   owner       = var.prefix                  # HashiCorp email (no @ symbol)
+
+#   create_aws_dns_zone   = true              # should be set to true
+  
+#   create_azure_dns_zone = false              # optional
+#   azure_location        = "westus"          # Azure creates a resource group
+#                                             #   which requires a region
+                                    
+#   create_gcp_dns_zone   = false              # optional
+#   gcp_project           = "robert-peteuil"  # name of your GCP project
+# }
 
 # Update DNS name
 resource "aws_route53_record" "splunk" {
